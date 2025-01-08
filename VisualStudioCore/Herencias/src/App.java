ad1fa14d5c290edae8bdee44fa1d50fc049d8749
COMPU-S030\daw1 <joelpereda99@gmail.com>
2025-01-08 09:41:21 +0100

    herencia

--- VisualStudioCore/Herencias/src/App.java
+++ VisualStudioCore/Herencias/src/App.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        
 
         String options = "";
 
         Astro [] astros = new Astro[100]; 
-        
-        Scanner sc = new Scanner(System.in);
-
+    
         System.out.println("\n Pon las medidas necesarias del Astro\n");
 
         System.out.println("Masa del cuerpo");
@@ -32,12 +33,14 @@ public class App {
         System.out.println(Info);
 
         System.out.println("¿que es tu astro?");
-        
+
+        sc = new Scanner(System.in);
             System.out.println("1) Planeta");
             System.out.println("2) Satelite");
             System.out.println("3) Ninguna de las dos");
             options = sc.nextLine();
 
+            sc = new Scanner(System.in);
             if (options.equals("1")){
                 System.out.println("Pon los datos necesarios para los planetas\n");
                 System.out.println("Distancia al Sol:");
@@ -47,29 +50,47 @@ public class App {
                 double orbitaSol = sc.nextDouble();
 
                 System.out.println("¿Tiene satelites?:");
-                boolean TieneSatelites = false;
+                
                 
                 System.out.println("escribe 'si' o 'no'");
+
+                sc = new Scanner(System.in);
+
+                boolean tieneSatelites = false;
+
                 String tieneSatelitesSi = sc.nextLine();
-                {
-                if (tieneSatelitesSi == "si"){
-                    TieneSatelites = true;
-                } else if (tieneSatelitesSi == "no"){
-                    TieneSatelites = false;
-                } else System.out.println("tienes que escribir 'si' o 'no'");
-                }while (tieneSatelitesSi == "si" && tieneSatelitesSi == "no");
-            }
-            if (options.equals("2")){
-                
-            }
-            if (options.equals("3")){
-                
-            }
+               
+                    
+                    if (tieneSatelitesSi.equalsIgnoreCase("si")){
+                        tieneSatelites = true;
+                    
+                    }else if (tieneSatelitesSi.equalsIgnoreCase("no")){
+                        tieneSatelites = false;
+                    
+                    }else System.out.println("tienes que escribir 'si' o 'no'");
 
+                Planetas Info2 = new Planetas (masa, diametro, rotacion, translacion, distancia,distanciaSol,orbitaSol,tieneSatelites); 
+                System.out.println(Info2);
 
-       
+            }if (options.equals("2")){
 
+                System.out.println("Pon los datos necesarios para los Satelites\n");
+                System.out.println("Distancia al Planeta:");
+                double distanciaPlaneta = sc.nextDouble();
 
+                System.out.println("Orbita al Planeta:");
+                double orbitaPlaneta = sc.nextDouble();
 
+                sc = new Scanner(System.in);
+                System.out.println("planeta al que pertenece");
+                String planetaPerteneciente = sc.nextLine();
+                Satelites Info3 = new Satelites(masa, diametro, rotacion, translacion, distancia,distanciaPlaneta,orbitaPlaneta,planetaPerteneciente);
+                System.out.println(Info3);
+
+            }if (options.equals("3")){
+            
+            System.out.println("ya estaria terminado");
+
+            }       
     }
 }         
