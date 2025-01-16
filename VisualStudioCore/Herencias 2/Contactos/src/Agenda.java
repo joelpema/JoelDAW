69e709e86adf08654e918630aa91278c2a9b0862
COMPU-S030\daw1 <joelpereda99@gmail.com>
2025-01-16 11:29:56 +0100

    a

--- VisualStudioCore/Herencias 2/Contactos/src/Agenda.java
+++ VisualStudioCore/Herencias 2/Contactos/src/Agenda.java
@@ -9,36 +9,52 @@ public class Agenda{
         this.contador = 0;
         
     }
-    public int buscaContacto(String nombre) {
-        for (int i = 0; i < contador; i++) {
-            if (contactos[i].getNombre().equalsIgnoreCase(nombre)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public boolean existeContacto(String nombre) {
-        return buscaContacto(nombre) != -1;
-    }
-
-
     public boolean añadirContacto(Contacto c) {
+        if (contador >= 20) {
+            System.out.println("La agenda está llena.");
+            return false;
+        }
         if (existeContacto(c.getNombre())) {
-            return false; 
+            System.out.println("El contacto ya existe.");
+            return false;
         }
         contactos[contador++] = c;
         return true;
     }
 
     public boolean eliminarContacto(String nombre) {
-        for (int i = 0; i < contador; i++) {
-            if (contactos[i].getNombre().equalsIgnoreCase(nombre)) {
-                System.arraycopy(contactos, i + 1, contactos, i, contador - i - 1);
-                contactos[--contador] = null;
-                return true;
+        int posicion = buscaContacto(nombre);
+        if (posicion == -1) {
+            System.out.println("El contacto no existe.");
+            return false;
+        }
+        for (int i = posicion; i < contador - 1; i++) {
+            contactos[i] = contactos[i + 1];
+        }
+        contactos[--contador] = null;
+        return true;
+    }
+
+    public boolean existeContacto(String nombre) {
+        return buscaContacto(nombre) != -1;
+    }
+
+    public void listarContactos() {
+        if (contador == 0) {
+            System.out.println("La agenda está vacía.");
+        } else {
+            for (int i = 0; i < contador; i++) {
+                System.out.println((i + 1) + ". " + contactos[i].getNombre() + " - " + contactos[i].getTelefono());
             }
         }
-        return false;
+    }
+
+    public int buscaContacto(String nombre) {
+        for (int i = 0; i < contador; i++) {
+            if (contactos[i].getNombre().equals(nombre)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
