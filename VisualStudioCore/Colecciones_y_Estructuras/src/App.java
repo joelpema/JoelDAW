e8757d2a4e6616c6e006558db54acc7aa0dba83a
COMPU-S030\daw1 <joelpereda99@gmail.com>
2025-01-29 08:38:42 +0100

    bien commiteao niño

--- VisualStudioCore/Colecciones_y_Estructuras/src/App.java
+++ VisualStudioCore/Colecciones_y_Estructuras/src/App.java
@@ -9,13 +9,16 @@ public class App {
         String option;
 
         do { 
+
+            sc = new Scanner(System.in);
+
             System.out.println("Selecciona una opcion de el menu\n");
             System.out.println("1. Añadir coche");
             System.out.println("2. Eliminar coche");
             System.out.println("3. Salir");
             option = sc.nextLine();
 
-            if (option == "1"){
+            if (option.equals("1")){
 
                 System.out.println("Añade la Matricula");
                 String matricula = sc.nextLine();
@@ -25,16 +28,24 @@ public class App {
                 String marca = sc.nextLine();
 
                 taller.aniadirElemento(color, marca, matricula);
-            }else if (option == "2"){
 
+            }else if (option.equals("2")){
 
-            }else if (option == "3"){   
+                System.out.println("Añada la matricula del coche que quieras eliminar");
+                String cocheEliminar = sc.nextLine();
+                taller.eliminaElemento(cocheEliminar);
 
+            }else if (option.equals("3")){   
 
+                taller.visualizaMatriculas();
+                taller.visualizaCoches();
+                taller.visualizaTaller();
+
+            }else {
+                System.out.println("Ingrese una opcion correcta en el menu");
             }
 
 
-            
         } while (!option.equals("3"));
 
 
--- VisualStudioCore/Colecciones_y_Estructuras/src/Taller.java
+++ VisualStudioCore/Colecciones_y_Estructuras/src/Taller.java
@@ -15,12 +15,43 @@ public class Taller {
 
         Coche coche = new Coche (color, marca);
 
-        if (matricula.contains(matricula)){
-        this.coches.put(matricula, coche);
-        }else if (matricula == matricula){
-            System.out.println("la matricula ya existe");
+        if (coches.containsKey(matricula)){
+        System.out.println("La matricula ya esta ingresada");
+        }else{
+        System.out.println("matricula ingresada correctamente");
+        coches.put(matricula, new Coche(color, marca));
         }
+    }    
 
+    public void eliminaElemento(String matricula){
+
+        if (coches.containsKey(matricula)) {
+            coches.remove(matricula);
+            System.out.println("Coche eliminado correctamente.");
+        } else {
+            System.out.println("Error: No existe un coche con esa matrícula.");
+        }
     }
 
+    public void visualizaMatriculas() {
+        System.out.println("Matrículas registradas: " + coches.keySet());
+    }
+
+    public void visualizaCoches() {
+        System.out.println("Coches en el taller:");
+        for (Coche coche : coches.values()) {
+            System.out.println(coche);
+        }
+    }
+
+    public void visualizaTaller() {
+        System.out.println("Detalles del taller:");
+        for (var entry : coches.entrySet()) {
+            System.out.println("Matrícula: " + entry.getKey() + ", " + entry.getValue());
+        }
+    }
 }
+
+
+
+
