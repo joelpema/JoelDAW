7f8dc910d9e6dde794f68ed23d316c2a8e365dc7
COMPU-S030\daw1 <joelpereda99@gmail.com>
2025-03-05 08:00:16 +0100

    commit

---
+++ VisualStudioCore/FicherosSerializables/src/App.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Bienvenido a mi libreria, escoge una opcion correcta en el menu");
+        do{
+
+            System.out.println("1. Crear Libro y registrarlo en la Biblioteca (ISBN único)");
+            System.out.println("2. Mostrar Libros existentes");
+            System.out.println("3. Eliminar Libro por ISBN");
+            System.out.println("4. Guardar Libros en el fichero");
+            System.out.println("5. Guardar y Salir");
+
+
+
+        }while ();
+
+
+
+
+
+    }
+}
---
+++ VisualStudioCore/FicherosSerializables/src/Libro.java
@@ -0,0 +1,65 @@
+
+import java.time.LocalDate;
+
+public class Libro {
+
+    private String isbn;
+    private String titulo;
+    private String Autor;
+    private LocalDate fechaPublicacion;
+
+    public Libro(String isbn, String titulo, String autor, LocalDate fechaPublicacion) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        Autor = autor;
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String autor) {
+        Autor = autor;
+    }
+
+    public LocalDate getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDate fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Libro{");
+        sb.append("isbn=").append(isbn);
+        sb.append(", titulo=").append(titulo);
+        sb.append(", Autor=").append(Autor);
+        sb.append(", fechaPublicacion=").append(fechaPublicacion);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    
+
+
+}
