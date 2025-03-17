31b29d42add2b6f5e666f4275628b80f1e86fe88
joelpema <joelpereda99@gmail.com>
2025-03-17 19:44:08 +0100

    commit

--- VisualStudioCore/FicherosCombinados/src/Inventario.java
+++ VisualStudioCore/FicherosCombinados/src/Inventario.java
@@ -1,24 +1,28 @@
-
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Inventario {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String opcion = "";
-        List<Producto> producto = new LinkedList<>();
+        List<Producto> productos = new LinkedList<>();
+
+        // Load data from files
+        cargarDatos(productos);
 
         System.out.println("Bienvenido a tu tienda de confianza!!!");
         
-        do { 
+        do {
             sc = new Scanner(System.in);
 
             System.out.println("1. Mostrar Productos en el Inventario.");
@@ -27,85 +31,138 @@ public class Inventario {
             System.out.println("4. Registrar producto en el Inventario");
             opcion = sc.nextLine();
 
-            if (opcion.equals("1")){
-
-                //fichero unicode
-
-                System.out.println("Mostrando Productos en el inventario\n");
-
-                try (FileReader file2 = new FileReader(".\\resources\\productos.csv"); BufferedReader reader = new BufferedReader(file2);) {
-                    String linea = reader.readLine();
-                    while(linea != null){
-                
-                String[] datos = linea.split("/");
-
-                System.out.println(linea);
-
-                linea = reader.readLine();
-
-                }   
-
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
-
-                //Ficheros binarios
-
-                int i = 0;
-                boolean eof = false;
-                try (FileInputStream fichero = new FileInputStream(".\\resources\\almacen.dat"); DataInputStream lector = new DataInputStream(fichero)) {
-
-                
-                    while(!eof){
-                        int cantidad = lector.readInt();
-                        double precio = lector.readDouble();
-                        int descuento = lector.readInt();
-                        int iva = lector.readInt();
-                        boolean aplicarDto = lector.readBoolean();
-
-                        
-
-                        System.out.println(cantidad);
-                        System.out.println(precio);
-                        System.out.println(descuento);
-                        System.out.println(iva);
-                        System.out.println(aplicarDto);
-
-                        if(i < producto.size()){
-                            i++;
-                        }
-                        
-                    }
-                    
-                } catch (EOFException e) {
-                    eof = true;
-                }catch (IOException e){
-                    System.out.println(e.getMessage());
-                }
-
-                for (Producto p : producto) {
-                    System.out.println(p);
-                }
-
-            }else if (opcion.equals("2")){
-
-                
-                
-
-            }else if (opcion.equals("3")){
-                
-                
-                
-                
-            }else if (opcion.equals("4")){
-
-
-                
-            }else {
-                System.out.println("Pon una opcion correcta en el menu");
+            switch (opcion) {
+                case "1":
+                    mostrarProductos(productos);
+                    break;
+                case "2":
+                    eliminarProducto(productos, sc);
+                    break;
+                case "3":
+                    guardarYSalir(productos);
+                    break;
+                case "4":
+                    registrarProducto(productos, sc);
+                    break;
+                default:
+                    System.out.println("Pon una opcion correcta en el menu");
             }
-
-            
         } while (!opcion.equals("3"));
     }
+
+    @Override
+    public String toString() {
+        return "Inventario []";
+    }
+
+    // ... (implement the methods below)
+}
+
+private static void cargarDatos(List<Producto> productos) {
+    cargarProductosCSV(productos);
+    cargarAlmacenDAT(productos);
+}
+
+private static void cargarProductosCSV(List<Producto> productos) {
+    try (BufferedReader reader = new BufferedReader(new FileReader(".\\resources\\productos.csv"))) {
+        String linea;
+        while ((linea = reader.readLine()) != null) {
+            String[] datos = linea.split("/");
+            if (datos.length >= 4) {
+                Producto producto = new Producto(datos[0], datos[1], datos[2], datos[3]);
+                productos.add(producto);
+            }
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+private static void cargarAlmacenDAT(List<Producto> productos) {
+    try (DataInputStream lector = new DataInputStream(new FileInputStream(".\\resources\\almacen.dat"))) {
+        for (Producto producto : productos) {
+            producto.setCantidad(lector.readInt());
+            producto.setPrecio(lector.readDouble());
+            producto.setDescuento(lector.readInt());
+            producto.setIva(lector.readInt());
+            producto.setAplicarDto(lector.readBoolean());
+        }
+    } catch (EOFException e) {
+        // End of file reached
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+private static void mostrarProductos(List<Producto> productos) {
+    System.out.println("Mostrando Productos en el inventario\n");
+    for (Producto producto : productos) {
+        System.out.println(producto);
+    }
+}
+
+private static void eliminarProducto(List<Producto> productos, Scanner sc) {
+    System.out.println("Introduce la referencia del producto que quieres eliminar");
+    String ref = sc.nextLine();
+
+    Producto eliminaProducto = null;
+    for (Producto producto : productos) {
+        if (Objects.equals(producto.getReferencia(), ref)) {
+            eliminaProducto = producto;
+            break;
+        }
+    }
+    if (eliminaProducto != null) {
+        productos.remove(eliminaProducto);
+        System.out.println("El producto se eliminó");
+    } else {
+        System.out.println("No se encontró un producto con esa referencia");
+    }
+}
+
+private static void guardarYSalir(List<Producto> productos) {
+    try (DataOutputStream escritor = new DataOutputStream(new FileOutputStream(".\\resources\\inventario.dat"))) {
+        for (Producto producto : productos) {
+            escritor.writeUTF(producto.getReferencia());
+            escritor.writeUTF(producto.getDescripcion());
+            escritor.writeUTF(producto.getTipo());
+            escritor.writeInt(producto.getCantidad());
+            escritor.writeDouble(producto.getPrecio());
+            escritor.writeInt(producto.getDescuento());
+            escritor.writeInt(producto.getIva());
+            escritor.writeBoolean(producto.isAplicarDto());
+        }
+        System.out.println("Datos guardados. Saliendo del programa.");
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+private static void registrarProducto(List<Producto> productos, Scanner sc) {
+    System.out.println("Introduce la referencia del nuevo producto:");
+    String ref = sc.nextLine();
+
+    if (productos.stream().anyMatch(p -> p.getReferencia().equals(ref))) {
+        System.out.println("Ya existe un producto con esa referencia.");
+        return;
+    }
+
+    System.out.println("Introduce la descripción:");
+    String descripcion = sc.nextLine();
+    System.out.println("Introduce el tipo:");
+    String tipo = sc.nextLine();
+    System.out.println("Introduce la cantidad:");
+    int cantidad = Integer.parseInt(sc.nextLine());
+    System.out.println("Introduce el precio:");
+    double precio = Double.parseDouble(sc.nextLine());
+    System.out.println("Introduce el descuento:");
+    int descuento = Integer.parseInt(sc.nextLine());
+    System.out.println("Introduce el IVA:");
+    int iva = Integer.parseInt(sc.nextLine());
+    System.out.println("¿Aplicar descuento? (true/false):");
+    boolean aplicarDto = Boolean.parseBoolean(sc.nextLine());
+
+    Producto nuevoProducto = new Producto(ref, descripcion, tipo, cantidad, precio, descuento, iva, aplicarDto);
+    productos.add(nuevoProducto);
+    System.out.println("Producto registrado con éxito.");
 }
--- VisualStudioCore/FicherosCombinados/src/Producto.java
+++ VisualStudioCore/FicherosCombinados/src/Producto.java
@@ -1,19 +1,31 @@
 public class Producto {
 
+    private static final long serialVersionUID = 4608803335361586627L;
+
     private String referencia;
-    private String nombre;
     private String descripcion;
     private String tipo;
     private int cantidad;
     private double precio;
     private int descuento;
     private int iva;
-    private boolean  aplicarDto;
+    private boolean aplicarDto;
+
+        
+    public Producto(String referencia, String descripcion, String tipo, String categoria) {
     
-    public Producto(String referencia, String nombre, String descripcion, String tipo, int cantidad, double precio,
-            int descuento, int iva, boolean aplicarDto) {
+    
+
+    
+
+    this.referencia = referencia;
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        
+    }
+
+    public Producto(String referencia, String descripcion, String tipo, int cantidad, double precio, int descuento, int iva, boolean aplicarDto) {
         this.referencia = referencia;
-        this.nombre = nombre;
         this.descripcion = descripcion;
         this.tipo = tipo;
         this.cantidad = cantidad;
@@ -23,58 +35,67 @@ public class Producto {
         this.aplicarDto = aplicarDto;
     }
 
+    
     public String getReferencia() {
         return referencia;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public String getDescripcion() {
-        return descripcion;
-    }
-    public String getTipo() {
-        return tipo;
-    }
-    public int getCantidad() {
-        return cantidad;
-    }
-    public double getPrecio() {
-        return precio;
-    }
-    public int getDescuento() {
-        return descuento;
-    }
-    public int getIva() {
-        return iva;
-    }
-    public boolean isAplicarDto() {
-        return aplicarDto;
     }
 
     public void setReferencia(String referencia) {
         this.referencia = referencia;
     }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+
+    public String getDescripcion() {
+        return descripcion;
     }
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
+    public String getTipo() {
+        return tipo;
+    }
+
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
+
+    public double getPrecio() {
+        return precio;
+    }
+
     public void setPrecio(double precio) {
         this.precio = precio;
     }
+
+    public int getDescuento() {
+        return descuento;
+    }
+
     public void setDescuento(int descuento) {
         this.descuento = descuento;
     }
+
+    public int getIva() {
+        return iva;
+    }
+
     public void setIva(int iva) {
         this.iva = iva;
     }
+
+    public boolean isAplicarDto() {
+        return aplicarDto;
+    }
+
     public void setAplicarDto(boolean aplicarDto) {
         this.aplicarDto = aplicarDto;
     }
@@ -84,7 +105,6 @@ public class Producto {
         StringBuilder sb = new StringBuilder();
         sb.append("Producto{");
         sb.append("Referencia=").append(referencia);
-        sb.append(", Nombre=").append(nombre);
         sb.append(", Descripcion=").append(descripcion);
         sb.append(", Tipo=").append(tipo);
         sb.append(", Cantidad=").append(cantidad);
@@ -95,9 +115,4 @@ public class Producto {
         sb.append('}');
         return sb.toString();
     }
-
-
-    
-    
-
 }
