9ee0c85b660ce3842cbe6d5280e336477275caef
COMPU-S022\daw2 <joelpereda99@gmail.com>
2025-11-03 15:02:04 +0100

    commit

--- DAW2/DWES/GestionPistas/src/main/java/com/example/GestionPistas/Controladores/nuevaPista.java
+++ DAW2/DWES/GestionPistas/src/main/java/com/example/GestionPistas/Controladores/nuevaPista.java
@@ -1,32 +1,42 @@
 package com.example.GestionPistas.Controladores;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.GestionPistas.Pista;
-// La anotación @Controller indica que esta clase es un controlador.
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
-import ch.qos.logback.core.model.Model;
+import com.example.GestionPistas.Pista;
+
+
 @Controller
 public class nuevaPista {
 
-    private List<Pista> pistas = new ArrayList<>(); 
+    private static List<Pista> listapista = new ArrayList<>();
 
-@GetMapping("/")
-@ResponseBody
-public String mostrarPista(Model model) {
+    @GetMapping("/")
+    public String mostrarPistas(Model model) {
 
-model.addAttribute("Pista", pistas);
+        model.addAttribute("pistas", listapista);
+        return "index";
 
-return "index";
+    }
+    
+    @PostMapping("/addPista")
+    public RedirectView añadirPista(
+        @RequestParam String nombrePista,
+        @RequestParam String  horas){
 
-}
+            Pista pista = new Pista(nombrePista, horas);
 
-@PostMapping("/addPista")
+            listapista.add(pista);
+
+        return new RedirectView("/");
+    }
+    
+
 
 }
