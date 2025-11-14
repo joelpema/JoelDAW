1b63e2c29b6cf1bb3b0e61eb500065dc06af7ad3
COMPU-S022\daw2 <joelpereda99@gmail.com>
2025-11-14 18:01:42 +0100

    commit

---
+++ DAW2/DWES/Tienda/src/main/java/com/example/Tienda/repositorio/PedidoRepository.java
@@ -0,0 +1,36 @@
+package com.example.Tienda.repositorio;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.Tienda.entidad.PedidoEntity;
+
+public class PedidoRepository {
+
+    public List<PedidoEntity> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    public Optional<PedidoEntity> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    public PedidoEntity save(PedidoEntity pedido) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    public boolean existsById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'existsById'");
+    }
+
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    
+}
