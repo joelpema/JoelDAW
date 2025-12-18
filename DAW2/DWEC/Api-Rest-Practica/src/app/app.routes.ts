1cea7710e71c3235dfff6b16d59ea5c0f99fd874
COMPU-S022\daw2 <joelpereda99@gmail.com>
2025-12-18 18:33:40 +0100

    commit de api rest

--- DAW2/DWEC/Api-Rest-Practica/src/app/app.routes.ts
+++ DAW2/DWEC/Api-Rest-Practica/src/app/app.routes.ts
@@ -1,6 +1,8 @@
+
 import { Routes } from '@angular/router';
 import { ComponentsHome } from './pages/components-home/components-home';
 import { ComponentsList } from './pages/components-list/components-list';
+
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
